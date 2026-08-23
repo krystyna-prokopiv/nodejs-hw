7893d397b88fbf8514118bdb780eb26c7adda067
Krystyna Prokopiv <krisrinaars237@gmail.com>
2026-08-23 17:38:51 +0300

    fix errorHandler

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -4,7 +4,7 @@ export const connectMongoDB = async () => {
   try {
     const mongoUrl = process.env.MONGO_URL;
     await mongoose.connect(mongoUrl);
-    console.log('✅ MongoDB successfully');
+    console.log('✅ MongoDB connection established successfully');
   } catch (error) {
     console.error('❌ Failed to connect to MongoDB:', error.message);
     process.exit(1); 
--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,14 @@
+import { HttpError } from 'http-errors';
 
-export const errorHandler = ((err, req, res, next) => {
-    const isProd = process.env.NODE_ENV === 'production';
-  res.status(500).json(isProd ? "Something went wrong." : err.message);
-});
+
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    const { status = 500 } = err;
+    return res.status(status).json({
+      message: err.message || err.name
+    });
+  }
+
+  const isProd = process.env.NODE_ENV === 'production';
+  res.status(500).json(isProd ? 'Something went wrong.' : err.message);
+};
