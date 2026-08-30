af404b15eb56ab109a147d55196ab4791b9f5751
Krystyna Prokopiv <krisrinaars237@gmail.com>
2026-08-30 14:27:56 +0300

    fixed

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -34,7 +34,7 @@ export const updateNoteSchema = {
     noteId: Joi.string().custom(objectIdValidator).required(),
   }),
   [Segments.BODY]: Joi.object({
-    title: Joi.string().min(1).required(),
+    title: Joi.string().min(1),
     content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
   }).min(1),
